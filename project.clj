c30aecc1de16bca45cde335327b3d1fd3f249f60
Christopher Miles <twitch@nervestaple.com>
2012-06-25 09:45:56 -0400

    Added unit tests to the project.

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.elasticsearch/elasticsearch "0.19.4"]
                  [cheshire "4.0.0"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.3.3"]]
+  :dev-dependencies [[org.clojure/tools.logging "0.2.3"]
+                     [commons-logging "1.1.1"]
+                     [log4j "1.2.16" :exclusions [javax.mail/mail
+                                                  javax.jms/jms
+                                                  com.sun.jdmk/jmxtools
+                                                  com.sun.jmx/jmxri]]
+                     [swank-clojure/swank-clojure "1.3.3"]]
   :main com.tnrglobal.plastic.core)
