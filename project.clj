0b6a4f4622ac2b6d96d1df041e39e5efd10d12de
Christopher M. Miles <miles@localhost.localdomain>
2012-08-03 14:31:37 -0400

    Incremented the version number.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject tnrglobal/plastic "0.5.2"
+(defproject tnrglobal/plastic "0.5.3"
   :Description "A library to ease development with Elasticsearch"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.elasticsearch/elasticsearch "0.19.4"]
                  [cheshire "4.0.0"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]
                      [swank-clojure/swank-clojure "1.3.3"]]
-  :main com.tnrglobal.plastic.core)
+  :main com.tnrglobal.plastic.core
+
+  :repositories {"releases"
+                 "http://nexus.nervestaple.com/content/groups/public"})
